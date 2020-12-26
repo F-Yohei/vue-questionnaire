7f5103622262546bd588fcf117256c25eda1c961
Yohei Fukudome <pt83s0502@gmail.com>
2020-12-26 21:21:22 +0900

    vue-routerのエラー解決。router.jsのコードを修正することにより解決。

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <BasicInfomation></BasicInfomation>
+    <!-- <BasicInfomation></BasicInfomation>
     <QuestionsPage></QuestionsPage>
-    <ConsultationPage></ConsultationPage>
-    <!-- <router-view></router-view> -->
+    <ConsultationPage></ConsultationPage> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import BasicInfomation from './components/BasicInfomation.vue';
-import QuestionsPage from './components/QuestionsPage.vue';
-import ConsultationPage from './components/ ConsultationFormPage.vue';
+// import BasicInfomation from './components/BasicInfomation.vue';
+// import QuestionsPage from './components/QuestionsPage.vue';
+// import ConsultationPage from './components/ConsultationFormPage.vue';
 import router from './router';
 
 export default {
   name: 'App',
   components: {
-    BasicInfomation,
-    QuestionsPage,
-    ConsultationPage
+    // BasicInfomation,
+    // QuestionsPage,
+    // ConsultationPage
   },
   router
 }
--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
 import { createRouter, createWebHistory } from 'vue-router';
 import BasicInfomation from './components/BasicInfomation.vue';
 import QuestionsPage from './components/QuestionsPage.vue';
 import ConsultationFormPage from './components/ConsultationFormPage.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,6 +13,4 @@ const router = createRouter({
     ],
 });
 
-const app = createApp(App);
-app.mount('#app');
-app.use(router);
+export default router;
